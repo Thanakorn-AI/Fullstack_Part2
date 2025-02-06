45aac27137f31bca25553adc51517525ea4200ff
Thanakorn Kitviriya <dukethanakorn@gmail.com>
2025-02-06 08:59:42 +0700

    Fixed courseinfo folder as regular directory

---
+++ courseinfo/src/App.jsx
@@ -0,0 +1,61 @@
+// introdemo/src/App.jsx
+import React from 'react';
+
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+};
+
+
+const Part = ({ name, exercises }) => {
+  return <p>{name} {exercises}</p>;
+};
+
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(part => (
+        <Part key={part.name} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <p>Number of exercises {total}</p>;
+};
+
+
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default App;
