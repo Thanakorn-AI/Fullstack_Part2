64fcfc8ecacace09e7edbb75e2f6f203bf9220ef
Thanakorn Kitviriya <dukethanakorn@gmail.comc>
2025-02-08 10:52:30 +0700

    Completed "Course information step 7"

--- courseinfo/src/components/Course.jsx
+++ courseinfo/src/components/Course.jsx
@@ -3,11 +3,19 @@ import React from 'react';
 import Header from './Header';
 import Content from './Content';
 
+const Total = ({ parts }) => {
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+    return <p>Total exercises: {total}</p>;
+  };
+  
+
+
 const Course = ({ course }) => {
   return (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
